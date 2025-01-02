d5bfb873d96d99932178d3ee54ac8d6e33795c4b
shygosh <shygosh@proton.me>
2025-01-03 02:13:12 +0700

    AK3: Rebrand

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Phantom kernel by shygosh
+kernel.string=Crimson kernel by shygosh
 do.devicecheck=0
 do.modules=0
 do.systemless=1
