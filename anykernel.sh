93cffc2ae528321a703228e1a611f7c7bc606522
shygosh <shygosh@proton.me>
2025-04-09 14:35:20 +0700

    AK3: Add support for A11

--- anykernel.sh
+++ anykernel.sh
@@ -11,7 +11,7 @@ do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=
-supported.versions=12-15
+supported.versions=11-15
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
